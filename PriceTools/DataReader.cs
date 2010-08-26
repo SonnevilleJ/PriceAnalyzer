42c22b6e37322d64dba96a39322affd8db60d91b
John Sonneville <SonnevilleJ@gmail.com>
2010-08-25 23:06:31 -0500

    Added methods to get data from database.

--- PriceTools/DataReader.cs
+++ PriceTools/DataReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,44 @@ namespace Sonneville.PriceTools
 {
     class DataReader
     {
-        
+        private const string _connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\dev\PriceAnalyzer\database.accdb;Persist Security Info=False;";
+
+        public DataSet GetPrice(string ticker, DateTime head, DateTime tail)
+        {
+            OleDbConnection conn = new OleDbConnection(_connString);
+            string sql = string.Format(
+                "SELECT D, O, H, L, C, Volume FROM {0} WHERE D BETWEEN #{1}# and #{2}#",
+                ticker,
+                head.ToShortDateString(),
+                tail.ToShortDateString());
+            DataSet ds = new DataSet();
+            OleDbDataAdapter adapter;
+            QueryDatabase(conn, sql, out adapter);
+            adapter.Fill(ds);
+            return ds;
+        }
+
+        public void QueryDatabase(OleDbConnection connection, string query, out OleDbDataAdapter da)
+        {
+            if(!string.IsNullOrEmpty(query) && !query.StartsWith("SELECT", StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new ArgumentException("Query is not a SELECT query.", "query");
+            }
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                da = new OleDbDataAdapter(query, connection);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+        }
     }
 }
---
+++ PriceTools/StockIndexes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sonneville.PriceTools
+{
+    public static class StockIndexes
+    {
+        public static string StandardAndPoors500
+        {
+            get
+            {
+                return "SPX";
+            }
+        }
+
+        public static string DowJonesIndustrialAverage
+        {
+            get
+            {
+                return "INDU";
+            }
+        }
+    }
+}
