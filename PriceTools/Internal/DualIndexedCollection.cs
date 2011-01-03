9a2e1406a5d2e7a5a89a6f7d41f9975b69ceffd4
John Sonneville <SonnevilleJ@gmail.com>
2011-01-03 00:32:30 -0600

    Implemented all methods for DualIndexedCollection but Add, Clear, Remove, and ISerializable methods.

--- PriceTools/Internal/DualIndexedCollection.cs
+++ PriceTools/Internal/DualIndexedCollection.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace Sonneville.PriceTools.Internal
 {
-    internal class DualIndexedCollection<T> : SortedSet<T>
+    [Serializable]
+    internal class DualIndexedCollection<T> : ICollection<T>, ISerializable
     {
-        #region Private Members
-
-        private ICollection _collection = new Dictionary<DateTime, T>();
-        private ICollection _map = new Dictionary<DateTime, int>();
-
-        #endregion
+        private ICollection _collection;
+        private ICollection _map;
 
         #region Constructors
 
         public DualIndexedCollection()
         {
+            _collection = new Dictionary<DateTime, T>();
+            _map = new Dictionary<DateTime, int>();
         }
 
         public DualIndexedCollection(int capacity)
         {
-            throw new NotImplementedException();
+            _collection = new Dictionary<DateTime, T>(capacity);
+            _map = new Dictionary<DateTime, int>(capacity);
         }
 
         #endregion
@@ -38,6 +40,132 @@ namespace Sonneville.PriceTools.Internal
         {
             get { throw new NotImplementedException(); }
             set { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region Implementation of IEnumerable
+
+        /// <summary>
+        ///   Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        ///   A <see cref = "T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>) _collection.GetEnumerator();
+        }
+
+        /// <summary>
+        ///   Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        ///   An <see cref = "T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Implementation of ICollection<T>
+
+        /// <summary>
+        ///   Adds an item to the <see cref = "T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <param name = "item">The object to add to the <see cref = "T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <exception cref = "T:System.NotSupportedException">The <see cref = "T:System.Collections.Generic.ICollection`1" /> is read-only.</exception>
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///   Removes all items from the <see cref = "T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <exception cref = "T:System.NotSupportedException">The <see cref = "T:System.Collections.Generic.ICollection`1" /> is read-only. </exception>
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///   Determines whether the <see cref = "T:System.Collections.Generic.ICollection`1" /> contains a specific value.
+        /// </summary>
+        /// <returns>
+        ///   true if <paramref name = "item" /> is found in the <see cref = "T:System.Collections.Generic.ICollection`1" />; otherwise, false.
+        /// </returns>
+        /// <param name = "item">The object to locate in the <see cref = "T:System.Collections.Generic.ICollection`1" />.</param>
+        public bool Contains(T item)
+        {
+            return _collection.Cast<T>().Contains(item);
+        }
+
+        /// <summary>
+        ///   Copies the elements of the <see cref = "T:System.Collections.Generic.ICollection`1" /> to an <see cref = "T:System.Array" />, starting at a particular <see cref = "T:System.Array" /> index.
+        /// </summary>
+        /// <param name = "array">The one-dimensional <see cref = "T:System.Array" /> that is the destination of the elements copied from <see cref = "T:System.Collections.Generic.ICollection`1" />. The <see cref = "T:System.Array" /> must have zero-based indexing.</param>
+        /// <param name = "arrayIndex">The zero-based index in <paramref name = "array" /> at which copying begins.</param>
+        /// <exception cref = "T:System.ArgumentNullException"><paramref name = "array" /> is null.</exception>
+        /// <exception cref = "T:System.ArgumentOutOfRangeException"><paramref name = "arrayIndex" /> is less than 0.</exception>
+        /// <exception cref = "T:System.ArgumentException"><paramref name = "array" /> is multidimensional.-or-The number of elements in the source <see cref = "T:System.Collections.Generic.ICollection`1" /> is greater than the available space from <paramref name = "arrayIndex" /> to the end of the destination <paramref name = "array" />.-or-Type <paramref name = "T" /> cannot be cast automatically to the type of the destination <paramref name = "array" />.</exception>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _collection.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        ///   Removes the first occurrence of a specific object from the <see cref = "T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <returns>
+        ///   true if <paramref name = "item" /> was successfully removed from the <see cref = "T:System.Collections.Generic.ICollection`1" />; otherwise, false. This method also returns false if <paramref name = "item" /> is not found in the original <see cref = "T:System.Collections.Generic.ICollection`1" />.
+        /// </returns>
+        /// <param name = "item">The object to remove from the <see cref = "T:System.Collections.Generic.ICollection`1" />.</param>
+        /// <exception cref = "T:System.NotSupportedException">The <see cref = "T:System.Collections.Generic.ICollection`1" /> is read-only.</exception>
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///   Gets the number of elements contained in the <see cref = "T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <returns>
+        ///   The number of elements contained in the <see cref = "T:System.Collections.Generic.ICollection`1" />.
+        /// </returns>
+        public int Count
+        {
+            get { return _collection.Count; }
+        }
+
+        /// <summary>
+        ///   Gets a value indicating whether the <see cref = "T:System.Collections.Generic.ICollection`1" /> is read-only.
+        /// </summary>
+        /// <returns>
+        ///   true if the <see cref = "T:System.Collections.Generic.ICollection`1" /> is read-only; otherwise, false.
+        /// </returns>
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        #endregion
+
+        #region Implementation of ISerializable
+
+        /// <summary>
+        ///   Populates a <see cref = "T:System.Runtime.Serialization.SerializationInfo" /> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name = "info">The <see cref = "T:System.Runtime.Serialization.SerializationInfo" /> to populate with data. </param>
+        /// <param name = "context">The destination (see <see cref = "T:System.Runtime.Serialization.StreamingContext" />) for this serialization. </param>
+        /// <exception cref = "T:System.Security.SecurityException">The caller does not have the required permission. </exception>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
