7eea6c7a9014b8b6b8b8097a6406023f0e2b151c
John Sonneville <SonnevilleJ@gmail.com>
2011-09-28 23:02:10 -0500

    Added partial RSI implementation.

---
+++ PriceTools/RelativeStrengthIndex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sonneville.PriceTools
+{
+    /// <summary>
+    /// A momentum oscillator which measures the speed of changes in price movements.
+    /// </summary>
+    public class RelativeStrengthIndex : Indicator
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a new RSI <see cref="IIndicator"/>.
+        /// </summary>
+        /// <param name="priceSeries">The <see cref="IPriceSeries"/> to measure.</param>
+        /// <param name="lookback">The lookback of this Indicator which specifies how many periods are required for the first indicator value.</param>
+        public RelativeStrengthIndex(IPriceSeries priceSeries, int lookback = 14)
+            : base(priceSeries, lookback)
+        {
+        }
+
+        #endregion
+
+        #region Overrides of Indicator
+
+        /// <summary>
+        /// Calculates a single value of this Indicator.
+        /// </summary>
+        /// <param name="index">The index of the value to calculate. The index of the current period is 0.</param>
+        /// <returns>The value of this Indicator for the given period.</returns>
+        protected override decimal Calculate(DateTime index)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ PriceToolsTest/RelativeStrengthIndexTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sonneville.PriceTools;
+using Sonneville.PriceTools.Services;
+using Sonneville.Utilities;
+
+namespace Sonneville.PriceToolsTest
+{
+    /// <summary>
+    /// Summary description for RelativeStrengthIndexTest
+    /// </summary>
+    [TestClass]
+    public class RelativeStrengthIndexTest
+    {
+        private static IPriceSeries GetPriceSeries()
+        {
+            return new YahooPriceHistoryCsvFile(new ResourceStream(TestData.DE_1_1_2011_to_3_15_2011_Daily_Yahoo)).PriceSeries;
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Settings.SetDefaultSettings();
+            Settings.CanConnectToInternet = false;
+        }
+
+        [TestMethod]
+        public void ResolutionDaysByDefault()
+        {
+            IPriceSeries priceSeries = GetPriceSeries();
+
+            RelativeStrengthIndex target = new RelativeStrengthIndex(priceSeries);
+
+            const Resolution expected = Resolution.Days;
+            Resolution actual = target.Resolution;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HeadTest()
+        {
+            DateTime date = new DateTime(2011, 1, 1);
+            IPriceSeries priceSeries = GetPriceSeries();
+
+            RelativeStrengthIndex target = new RelativeStrengthIndex(priceSeries);
+
+            DateTime expected = date.AddDays(target.Lookback);
+            DateTime actual = target.Head;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void QueryBeforeHeadThrowsException()
+        {
+            IPriceSeries series = GetPriceSeries();
+            RelativeStrengthIndex rsi = new RelativeStrengthIndex(series);
+
+            var result = rsi[rsi.Head.Subtract(new TimeSpan(1))];
+        }
+    }
+}
