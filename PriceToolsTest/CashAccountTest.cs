ebf7fc0df9a3670c15ec52769806bf5a872b3d63
John Sonneville <SonnevilleJ@gmail.com>
2011-12-08 11:12:15 -0600

    Added tests written in September 2011, never checked in.

--- PriceToolsTest/CashAccountTest.cs
+++ PriceToolsTest/CashAccountTest.cs
@@ -1,7 +1,6 @@
 ﻿using Sonneville.PriceTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Sonneville.Utilities;
 
 namespace Sonneville.PriceToolsTest
 {
@@ -19,12 +18,12 @@ namespace Sonneville.PriceToolsTest
         public void DepositTest()
         {
             ICashAccount target = new CashAccount();
-            DateTime dateTime = new DateTime(2010, 1, 16);
+            var dateTime = new DateTime(2010, 1, 16);
             const decimal amount = 500.00m;
             target.Deposit(dateTime, amount);
 
             const decimal expectedValue = amount;
-            decimal actualValue = target.GetCashBalance(dateTime);
+            var actualValue = target.GetCashBalance(dateTime);
             Assert.AreEqual(expectedValue, actualValue);
         }
         
@@ -35,13 +34,118 @@ namespace Sonneville.PriceToolsTest
         public void WithdrawTest()
         {
             ICashAccount target = new CashAccount();
-            DateTime dateTime = new DateTime(2010, 1, 16);
+            var dateTime = new DateTime(2010, 1, 16);
             const decimal amount = 500.00m;
             target.Deposit(dateTime, amount);
             target.Withdraw(dateTime, amount);
 
             const decimal expectedValue = 0.00m;
-            decimal actualValue = target.GetCashBalance(dateTime);
+            var actualValue = target.GetCashBalance(dateTime);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void RoundingCountTest1()
+        {
+            ICashAccount target = new CashAccount();
+            var dateTime = new DateTime(2011, 9, 16);
+            const decimal amount = 500.00m;
+            const int iterations = 10000;     // $0.05 transactions
+
+            for (var i = 0; i < iterations; i++)
+            {
+                // workaround for bug with Entity Framework
+                // EF will call GetHashCode, which returns a random number (I think).
+                // Occasionally, two Transaction objects will return the same result for GetHashCode.
+                // This results in EF getting confused and thinking the new object is already in the collection
+                // and EF will not add the new object to the collection, thus changing the final count.
+                var settlementDate = dateTime.AddTicks(i);
+
+                var dividend = new Deposit { Amount = amount / iterations, SettlementDate = settlementDate };
+                target.Deposit(dividend);
+            }
+            Assert.AreEqual(iterations, target.Transactions.Count);
+        }
+
+        [TestMethod]
+        public void RoundingCountTest2()
+        {
+            ICashAccount target = new CashAccount();
+            var dateTime = new DateTime(2011, 9, 16);
+            const decimal amount = 500.00m;
+            const int iterations = 10000;     // $0.05 transactions
+
+            for (var i = 0; i < iterations; i++)
+            {
+                // workaround for bug with Entity Framework
+                // EF will call GetHashCode, which returns a random number (I think).
+                // Occasionally, two Transaction objects will return the same result for GetHashCode.
+                // This results in EF getting confused and thinking the new object is already in the collection
+                // and EF will not add the new object to the collection, thus changing the final count.
+                var settlementDate = dateTime.AddTicks(i);
+
+                var dividend = new Deposit { Amount = amount / iterations, SettlementDate = settlementDate };
+                target.Deposit(dividend);
+            }
+
+            for (var j = 0; j < iterations; j++)
+            {
+                // see comment above about bug with Entity Framework
+                var settlementDate = dateTime.AddTicks(j);
+
+                var withdrawal = new Withdrawal { Amount = amount / iterations, SettlementDate = settlementDate };
+                target.Withdraw(withdrawal);
+            }
+            Assert.AreEqual(iterations * 2, target.Transactions.Count);
+        }
+
+        [TestMethod]
+        public void RoundingBalanceTest()
+        {
+            ICashAccount target = new CashAccount();
+            var dateTime = new DateTime(2011, 9, 16);
+            const decimal amount = 500.00m;
+            const int iterations = 10000;     // $0.05 transactions
+
+            for (var i = 0; i < iterations; i++)
+            {
+                // workaround for bug with Entity Framework
+                // EF will call GetHashCode, which returns a random number (I think).
+                // Occasionally, two Transaction objects will return the same result for GetHashCode.
+                // This results in EF getting confused and thinking the new object is already in the collection
+                // and EF will not add the new object to the collection, thus changing the final count.
+                var settlementDate = dateTime.AddTicks(i);
+
+                var dividend = new Deposit { Amount = amount / iterations, SettlementDate = settlementDate };
+                target.Deposit(dividend);
+            }
+
+            for (var j = 0; j < iterations; j++)
+            {
+                // see comment above about bug with Entity Framework
+                var settlementDate = dateTime.AddTicks(j);
+
+                var withdrawal = new Withdrawal { Amount = amount / iterations, SettlementDate = settlementDate };
+                target.Withdraw(withdrawal);
+            }
+
+            const decimal expected = 0.00m;
+            var actual = target.GetCashBalance(dateTime);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanWithdrawDividendsTest()
+        {
+            ICashAccount target = new CashAccount();
+            var dateTime = new DateTime(2010, 1, 16);
+            const decimal amount = 500.00m;
+            var dividendReceipt = new DividendReceipt {Amount = amount, SettlementDate = dateTime};
+            target.Deposit(dividendReceipt);
+            target.Withdraw(dateTime, amount);
+
+            const decimal expectedValue = 0.00m;
+            var actualValue = target.GetCashBalance(dateTime);
             Assert.AreEqual(expectedValue, actualValue);
         }
 
@@ -52,13 +156,13 @@ namespace Sonneville.PriceToolsTest
         public void TransactionsTest()
         {
             ICashAccount target = new CashAccount();
-            DateTime dateTime = new DateTime(2010, 1, 16);
+            var dateTime = new DateTime(2010, 1, 16);
             const decimal amount = 500.00m;
             target.Deposit(dateTime, amount);
             target.Withdraw(dateTime, amount);
 
             const int expectedTransactions = 2;
-            int actualTransactions = target.Transactions.Count;
+            var actualTransactions = target.Transactions.Count;
             Assert.AreEqual(expectedTransactions, actualTransactions);
         }
     }
