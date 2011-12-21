c33af7196bffecb49ae747a1ba7a840d394b621c
John Sonneville <SonnevilleJ@gmail.com>
2011-12-21 14:25:40 -0600

    Added first test for CommissionSchedule.

---
+++ TradingTest/CommissionScheduleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sonneville.PriceTools;
+using Sonneville.PriceTools.Trading;
+
+namespace Sonneville.TradingTest
+{
+    [TestClass]
+    public class CommissionScheduleTest
+    {
+        [TestMethod]
+        public void DefaultCommissionBuy()
+        {
+            var target = new FidelityCommissionSchedule();
+            var order = new Order(DateTime.Now, DateTime.Today.AddDays(1), OrderType.Buy, "DE", 1, 100);
+
+            const decimal expected = 0.00m;
+            var actual = target.PriceCheck(order);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
