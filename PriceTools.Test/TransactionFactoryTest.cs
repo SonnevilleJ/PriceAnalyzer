465b05d9565a266f25bc865e4eb90adc94546ccb
John Sonneville <SonnevilleJ@gmail.com>
2012-01-18 15:25:17 -0600

    Refactored TransactionFactory tests.

--- PriceTools.Test/TransactionFactoryTest.cs
+++ PriceTools.Test/TransactionFactoryTest.cs
@@ -8,190 +8,204 @@ namespace Sonneville.PriceTools.Test
     [TestClass]
     public class TransactionFactoryTest
     {
-        [TestMethod]
-        public void ConstructDepositTest()
+        public abstract class CashTransactionTestsBase
         {
-            var date = new DateTime(2012, 1, 17);
-            const decimal amount = 10000.00m;
+            public abstract void SerializeTest();
 
-            var target = TransactionFactory.ConstructDeposit(date, amount);
+            /// <summary>
+            ///A test for Amount
+            ///</summary>
+            public abstract void AmountInvertedTest();
 
-            Assert.AreEqual(OrderType.Deposit, target.OrderType);
-            Assert.AreEqual(date, target.SettlementDate);
-            Assert.AreEqual(Math.Abs(amount), Math.Abs(target.Amount));
-        }
+            /// <summary>
+            ///A test for SettlementDate
+            ///</summary>
+            public abstract void SettlementDateTest();
 
-        [TestMethod]
-        public void ConstructWithdrawalTest()
-        {
-            var date = new DateTime(2012, 1, 17);
-            const decimal amount = 10000.00m;
+            /// <summary>
+            ///A test for OrderType
+            ///</summary>
+            public abstract void OrderTypeTest();
 
-            var target = TransactionFactory.ConstructWithdrawal(date, amount);
+            /// <summary>
+            ///A test for Amount
+            ///</summary>
+            public abstract void AmountCorrectTest();
 
-            Assert.AreEqual(OrderType.Withdrawal, target.OrderType);
-            Assert.AreEqual(date, target.SettlementDate);
-            Assert.AreEqual(Math.Abs(amount), Math.Abs(target.Amount));
+            protected static void CashTransactionSerializeTest(OrderType transactionType)
+            {
+                var date = new DateTime(2012, 1, 17);
+                const decimal amount = 10000.00m;
+
+                var target = TransactionFactory.ConstructCashTransaction(transactionType, date, amount);
+
+                var xml = Serializer.SerializeToXml(target);
+                var result = Serializer.DeserializeFromXml<ICashTransaction>(xml);
+
+                TestUtilities.AssertSameState(target, result);
+            }
+
+            protected static void CashTransactionSettlementDateTest(OrderType transactionType)
+            {
+                var date = new DateTime(2000, 1, 1);
+                const decimal price = 2.00m;
+
+                var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
+
+                var expected = date;
+                var actual = target.SettlementDate;
+                Assert.AreEqual(expected, actual);
+            }
+
+            protected static void CashTransactionOrderTypeTest(OrderType transactionType)
+            {
+                var date = new DateTime(2000, 1, 1);
+                const decimal price = 2.00m;
+
+                var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
+
+                var expected = transactionType;
+                var actual = target.OrderType;
+                Assert.AreEqual(expected, actual);
+            }
+
+            protected static void CashTransactionAmountInvertedTest(OrderType transactionType)
+            {
+                var date = new DateTime(2000, 1, 1);
+                var price = GetInversePrice(transactionType);
+
+                var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
+
+                var expected = GetCorrectPrice(transactionType);
+                var actual = target.Amount;
+                Assert.AreEqual(expected, actual);
+            }
+
+            protected static void CashTransactionAmountCorrectTest(OrderType transactionType)
+            {
+                var date = new DateTime(2000, 1, 1);
+                var price = GetCorrectPrice(transactionType);
+
+                var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
+
+                var expected = GetCorrectPrice(transactionType);
+                var actual = target.Amount;
+                Assert.AreEqual(expected, actual);
+            }
+
+            private static decimal GetCorrectPrice(OrderType transactionType)
+            {
+                switch (transactionType)
+                {
+                    case OrderType.DividendReceipt:
+                        return 2.00m;
+                    case OrderType.Withdrawal:
+                        return -2.00m;
+                    default:
+                        return 0.00m;
+                }
+            }
+
+            private static decimal GetInversePrice(OrderType transactionType)
+            {
+                return -GetCorrectPrice(transactionType);
+            }
         }
 
         [TestClass]
-        public class DividendReceiptTests
+        public class WithdrawalTests : CashTransactionTestsBase
         {
+            private const OrderType TransactionType = OrderType.Withdrawal;
+
             [TestMethod]
-            public void SerializeTest()
+            public override void SerializeTest()
             {
-                CashTransactionSerializeTest(OrderType.DividendReceipt);
+                CashTransactionSerializeTest(TransactionType);
             }
 
             /// <summary>
             ///A test for Amount
             ///</summary>
             [TestMethod]
-            public void DividendReceiptAmountNegativeTest()
+            public override void AmountInvertedTest()
             {
-                CashTransactionAmountInvertedTest(OrderType.DividendReceipt);
+                CashTransactionAmountInvertedTest(TransactionType);
             }
 
             /// <summary>
             ///A test for SettlementDate
             ///</summary>
             [TestMethod]
-            public void DividendReceiptSettlementDateTest()
+            public override void SettlementDateTest()
             {
-                CashTransactionSettlementDateTest(OrderType.DividendReceipt);
+                CashTransactionSettlementDateTest(TransactionType);
             }
 
             /// <summary>
             ///A test for OrderType
             ///</summary>
             [TestMethod]
-            public void DividendReceiptOrderTypeTest()
+            public override void OrderTypeTest()
             {
-                CashTransactionOrderTypeTest(OrderType.DividendReceipt);
+                CashTransactionOrderTypeTest(TransactionType);
             }
 
             /// <summary>
             ///A test for Amount
             ///</summary>
             [TestMethod]
-            public void DividendReceiptAmountPositiveTest()
+            public override void AmountCorrectTest()
             {
-                CashTransactionAmountCorrectTest(OrderType.DividendReceipt);
+                CashTransactionAmountCorrectTest(TransactionType);
             }
         }
 
-        #region Test runners
-
-        private static void CashTransactionSerializeTest(OrderType transactionType)
+        [TestClass]
+        public class DividendReceiptTests : CashTransactionTestsBase
         {
-            var date = new DateTime(2012, 1, 17);
-            const decimal amount = 10000.00m;
+            private const OrderType TransactionType = OrderType.DividendReceipt;
 
-            var target = TransactionFactory.ConstructCashTransaction(transactionType, date, amount);
-
-            var xml = Serializer.SerializeToXml(target);
-            var result = Serializer.DeserializeFromXml<ICashTransaction>(xml);
-
-            TestUtilities.AssertSameState(target, result);
-        }
-
-        private static void CashTransactionSettlementDateTest(OrderType transactionType)
-        {
-            var date = new DateTime(2000, 1, 1);
-            const decimal price = 2.00m;
-
-            var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
-
-            var expected = date;
-            var actual = target.SettlementDate;
-            Assert.AreEqual(expected, actual);
-        }
-
-        private static void CashTransactionOrderTypeTest(OrderType transactionType)
-        {
-            var date = new DateTime(2000, 1, 1);
-            const decimal price = 2.00m;
-
-            var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
-
-            var expected = transactionType;
-            var actual = target.OrderType;
-            Assert.AreEqual(expected, actual);
-        }
-
-        private static void CashTransactionAmountInvertedTest(OrderType transactionType)
-        {
-            var date = new DateTime(2000, 1, 1);
-            var price = GetInversePrice(transactionType);
-
-            var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
-
-            var expected = GetCorrectPrice(transactionType);
-            var actual = target.Amount;
-            Assert.AreEqual(expected, actual);
-        }
-
-        private static void CashTransactionAmountCorrectTest(OrderType transactionType)
-        {
-            var date = new DateTime(2000, 1, 1);
-            var price = GetCorrectPrice(transactionType);
-
-            var target = TransactionFactory.ConstructCashTransaction(transactionType, date, price);
-
-            var expected = GetCorrectPrice(transactionType);
-            var actual = target.Amount;
-            Assert.AreEqual(expected, actual);
-        }
-
-        private static decimal GetCorrectPrice(OrderType transactionType)
-        {
-            switch (transactionType)
+            [TestMethod]
+            public override void SerializeTest()
             {
-                case OrderType.DividendReceipt:
-                    return 2.00m;
-                default:
-                    return 0.00m;
+                CashTransactionSerializeTest(TransactionType);
             }
-        }
 
-        private static decimal GetInversePrice(OrderType transactionType)
-        {
-            return -GetCorrectPrice(transactionType);
-        }
+            /// <summary>
+            ///A test for Amount
+            ///</summary>
+            [TestMethod]
+            public override void AmountInvertedTest()
+            {
+                CashTransactionAmountInvertedTest(TransactionType);
+            }
 
-        #endregion
+            /// <summary>
+            ///A test for SettlementDate
+            ///</summary>
+            [TestMethod]
+            public override void SettlementDateTest()
+            {
+                CashTransactionSettlementDateTest(TransactionType);
+            }
 
-        [TestMethod]
-        public void ConstructCashTransactionDepositTest()
-        {
-            var date = new DateTime(2012, 1, 17);
-            const decimal amount = 10000.00m;
+            /// <summary>
+            ///A test for OrderType
+            ///</summary>
+            [TestMethod]
+            public override void OrderTypeTest()
+            {
+                CashTransactionOrderTypeTest(TransactionType);
+            }
 
-            var expected = TransactionFactory.ConstructDeposit(date, amount);
-            var actual = TransactionFactory.ConstructCashTransaction(OrderType.Deposit, date, amount);
-            TestUtilities.AssertSameState(expected, actual);
-        }
-
-        [TestMethod]
-        public void ConstructCashTransactionWithdrawalTest()
-        {
-            var date = new DateTime(2012, 1, 17);
-            const decimal amount = 10000.00m;
-
-            var expected = TransactionFactory.ConstructWithdrawal(date, amount);
-            var actual = TransactionFactory.ConstructCashTransaction(OrderType.Withdrawal, date, amount);
-            TestUtilities.AssertSameState(expected, actual);
-        }
-
-        [TestMethod]
-        public void ConstructCashTransactionDividendReceiptTest()
-        {
-            var date = new DateTime(2012, 1, 17);
-            const decimal amount = 10000.00m;
-
-            var expected = TransactionFactory.ConstructDividendReceipt(date, amount);
-            var actual = TransactionFactory.ConstructCashTransaction(OrderType.DividendReceipt, date, amount);
-            TestUtilities.AssertSameState(expected, actual);
+            /// <summary>
+            ///A test for Amount
+            ///</summary>
+            [TestMethod]
+            public override void AmountCorrectTest()
+            {
+                CashTransactionAmountCorrectTest(TransactionType);
+            }
         }
     }
 }
