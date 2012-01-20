f1fedd6f93aad27910e3f8c8d6bb5336bb0e0d2b
John Sonneville <SonnevilleJ@gmail.com>
2012-01-20 10:03:31 -0600

    Fixed comments.

--- Data/PriceColumn.cs
+++ Data/PriceColumn.cs
@@ -11,37 +11,37 @@
         None = 0,
 
         /// <summary>
-        /// Represents the Date column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the Date column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         Date,
 
         /// <summary>
-        /// Represents the Open column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the Open column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         Open,
 
         /// <summary>
-        /// Represents the High column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the High column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         High,
 
         /// <summary>
-        /// Represents the Low column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the Low column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         Low,
 
         /// <summary>
-        /// Represents the Close column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the Close column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         Close,
 
         /// <summary>
-        /// Represents the Volume column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the Volume column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         Volume,
 
         /// <summary>
-        /// Represents the Dividends column in a table of <see cref="StaticPricePeriod"/> elements.
+        /// Represents the Dividends column in a table of static <see cref="PricePeriod"/> data.
         /// </summary>
         Dividends
     }
