62dd7ebf97cbe2681f1f2422a329c663bc6bf2f1
John Sonneville <SonnevilleJ@gmail.com>
2012-02-19 12:18:04 -0600

    Moved tests for Holdings. Created Holding extensions class and tests.

---
+++ PriceTools.Test/HoldingCalculationTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sonneville.PriceTools.Test
+{
+    [TestClass]
+    public class HoldingCalculationTests
+    {
+        [TestMethod]
+        public void GrossProfitTest()
+        {
+            var target = new Holding {Shares = 5, OpenPrice = 10, ClosePrice = 20, OpenCommission = 2, CloseCommission = 3};
+
+            var expected = ((decimal) target.Shares*(target.ClosePrice - target.OpenPrice));
+            var actual = target.GrossProfit();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NetProfitTest()
+        {
+            var target = new Holding {Shares = 5, OpenPrice = 10, ClosePrice = 20, OpenCommission = 2, CloseCommission = 3};
+
+            var expected = ((decimal) target.Shares * (target.ClosePrice - target.OpenPrice)) - target.OpenCommission - target.CloseCommission;
+            var actual = target.NetProfit();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PriceTools/HoldingExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Sonneville.PriceTools
+{
+    /// <summary>
+    /// A class which holds extension methods for <see cref="IHolding"/>.
+    /// </summary>
+    public static class HoldingExtensions
+    {
+        /// <summary>
+        /// Calculates the profit of a holding, before commissions.
+        /// </summary>
+        /// <param name="holding"></param>
+        /// <returns></returns>
+        public static decimal GrossProfit(this IHolding holding)
+        {
+            return (holding.ClosePrice - holding.OpenPrice) * (decimal) holding.Shares;
+        }
+
+        /// <summary>
+        /// Calculates the profit of a holding, after commissions.
+        /// </summary>
+        /// <param name="holding"></param>
+        /// <returns></returns>
+        public static decimal NetProfit(this IHolding holding)
+        {
+            return holding.GrossProfit() - holding.OpenCommission - holding.CloseCommission;
+        }
+    }
+}
