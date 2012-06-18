f331e5fb56d47ae18870b47b04949f033bd18052
John Sonneville <SonnevilleJ@gmail.com>
2012-06-18 22:46:01 +0200

    Implemented IsEqual for PricePeriod.

---
+++ PriceTools.Test/PricePeriodExtentionsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sonneville.PriceTools.Test
+{
+    [TestClass]
+    public class PricePeriodExtentionsTest
+    {
+        [TestMethod]
+        public void IsEqualDifferentData()
+        {
+            var head = new DateTime(2011, 3, 13);
+            var tail = head.AddDays(1);
+            const decimal close = 100.00m;
+
+            var period1 = PricePeriodFactory.CreateStaticPricePeriod(head, tail, close);
+            var period2 = PricePeriodFactory.CreateStaticPricePeriod(head.AddDays(1), tail.AddDays(1), close);
+
+            Assert.IsFalse(period1.IsEqual(period2));
+        }
+
+        [TestMethod]
+        public void IsEqualSameData()
+        {
+            var head = new DateTime(2011, 3, 13);
+            var tail = head.AddDays(1);
+            const decimal close = 100.00m;
+
+            var period1 = PricePeriodFactory.CreateStaticPricePeriod(head, tail, close);
+            var period2 = PricePeriodFactory.CreateStaticPricePeriod(head, tail, close);
+
+            Assert.IsTrue(period1.IsEqual(period2));
+        }
+        
+        [TestMethod]
+        public void IsEqualDifferentImplementations()
+        {
+            var head = new DateTime(2011, 3, 13);
+            var tail = head.AddDays(1);
+            const decimal close = 100.00m;
+
+            var period1 = PricePeriodFactory.CreateStaticPricePeriod(head, tail, close);
+            var period2 = PricePeriodFactory.ConstructTickedPricePeriod(new List<PriceTick>
+                                                                            {
+                                                                                PriceTickFactory.ConstructPriceTick(head, close),
+                                                                                PriceTickFactory.ConstructPriceTick(tail, close)
+                                                                            });
+
+            Assert.IsFalse(period1.IsEqual(period2));
+        }
+    }
+}
---
+++ PriceTools/PricePeriodExtentions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sonneville.PriceTools
+{
+    /// <summary>
+    /// A class storing extention methods 
+    /// </summary>
+    public static class PricePeriodExtentions
+    {
+        /// <summary>
+        /// Returns a value indicating whether a target <see cref="PricePeriod"/> is equal to the original <see cref="PricePeriod"/>.
+        /// </summary>
+        /// <remarks>This method only considers the data within the <see cref="PricePeriod"/> and ignores the datatype implementing the <see cref="PricePeriod"/>.</remarks>
+        public static bool IsEqual(this PricePeriod original, PricePeriod target)
+        {
+            if (original == null || target == null) return false;
+
+            return original.Resolution == target.Resolution &&
+                   original.Head == target.Head &&
+                   original.Tail == target.Tail &&
+                   original.Open == target.Open &&
+                   original.High == target.High &&
+                   original.Low == target.Low &&
+                   original.Close == target.Close &&
+                   original.Volume == target.Volume;
+        }
+    }
+}
