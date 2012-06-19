03b5628d439cda8eb6b186fb64a43f52b26cf734
John Sonneville <SonnevilleJ@gmail.com>
2012-06-19 21:45:16 +0200

    Implemented IsEquivalent for Transaction.

--- PriceTools.Test/TransactionExtentionsTest.cs
+++ PriceTools.Test/TransactionExtentionsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Sonneville.PriceTools.Test
@@ -32,6 +33,101 @@ namespace Sonneville.PriceTools.Test
             var t2 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares * 2, price);
 
             Assert.IsFalse(t1.IsEqual(t2));
+        }
+
+        [TestMethod]
+        public void EnumerableIsEquivalentWithSameData()
+        {
+            const string ticker = "DE";
+            var settlementDate = new DateTime(2012, 6, 19);
+            const decimal shares = 5.0m;
+            const decimal price = 100.00m;
+
+            var t1 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t2 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t3 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t4 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+
+            var list1 = new List<Transaction> {t1, t2};
+            var list2 = new List<Transaction> {t3, t4};
+
+            Assert.IsTrue(list1.IsEquivalent(list2));
+        }
+
+        [TestMethod]
+        public void EnumerableIsEquivalentWithDifferentData()
+        {
+            const string ticker = "DE";
+            var settlementDate = new DateTime(2012, 6, 19);
+            const decimal shares = 5.0m;
+            const decimal price = 100.00m;
+
+            var t1 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t2 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t3 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares + 1, price);
+            var t4 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+
+            var list1 = new List<Transaction> { t1, t2 };
+            var list2 = new List<Transaction> { t3, t4 };
+
+            Assert.IsFalse(list1.IsEquivalent(list2));
+        }
+
+        [TestMethod]
+        public void EnumerableIsEquivalentWithExtraTransaction()
+        {
+            const string ticker = "DE";
+            var settlementDate = new DateTime(2012, 6, 19);
+            const decimal shares = 5.0m;
+            const decimal price = 100.00m;
+
+            var t1 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t2 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t3 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t4 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t5 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+
+            var list1 = new List<Transaction> { t1, t2 };
+            var list2 = new List<Transaction> { t3, t4, t5 };
+
+            Assert.IsFalse(list1.IsEquivalent(list2));
+        }
+
+        [TestMethod]
+        public void EnumerableIsEquivalentWithMissingTransaction()
+        {
+            const string ticker = "DE";
+            var settlementDate = new DateTime(2012, 6, 19);
+            const decimal shares = 5.0m;
+            const decimal price = 100.00m;
+
+            var t1 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t2 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t3 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+
+            var list1 = new List<Transaction> { t1, t2 };
+            var list2 = new List<Transaction> { t3 };
+
+            Assert.IsFalse(list1.IsEquivalent(list2));
+        }
+
+        [TestMethod]
+        public void EnumerableIsEquivalentOrderCheck()
+        {
+            const string ticker = "DE";
+            var settlementDate = new DateTime(2012, 6, 19);
+            const decimal shares = 5.0m;
+            const decimal price = 100.00m;
+
+            var t1 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares * 2, price);
+            var t2 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t3 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares, price);
+            var t4 = TransactionFactory.ConstructBuy(ticker, settlementDate, shares * 2, price);
+
+            var list1 = new List<Transaction> { t1, t2 };
+            var list2 = new List<Transaction> { t3, t4 };
+
+            Assert.IsTrue(list1.IsEquivalent(list2));
         }
     }
 }
--- PriceTools/TransactionExtentions.cs
+++ PriceTools/TransactionExtentions.cs
@@ -1,4 +1,7 @@
-﻿namespace Sonneville.PriceTools
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sonneville.PriceTools
 {
     /// <summary>
     /// A class storing extension methods for <see cref="Transaction"/> objects.
@@ -6,7 +9,7 @@
     public static class TransactionExtentions
     {
         /// <summary>
-        /// Returns a value indicating whether a target <see cref="PricePeriod"/> is equal to the original <see cref="PricePeriod"/>.
+        /// Returns a value indicating whether a target <see cref="Transaction"/> is equal to the original <see cref="Transaction"/>.
         /// </summary>
         public static bool IsEqual(this Transaction original, Transaction target)
         {
@@ -42,6 +45,19 @@
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether a target <see cref="IEnumerable{Transaction}"/> is equal to the original <see cref="IEnumerable{Transaction}"/>.
+        /// </summary>
+        /// <param name="original"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static bool IsEquivalent(this IEnumerable<Transaction> original, IEnumerable<Transaction> target)
+        {
+            var oParallel = original.AsParallel();
+            var tParallel = target.AsParallel();
+            return oParallel.All(transaction => oParallel.Where(t => t.IsEqual(transaction)).Count() == tParallel.Where(t => t.IsEqual(transaction)).Count());
         }
     }
 }
