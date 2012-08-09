6177b9a9fc1b453822a4c97e5cf7ffeb388b828d
John Sonneville <SonnevilleJ@gmail.com>
2012-08-09 00:55:48 -0500

    Added ContinuousTradingEngine example.

---
+++ AutomatedTrading.Test/ContinuousTradingEngineTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sonneville.PriceTools.AutomatedTrading.Test
+{
+    [TestClass]
+    public class ContinuousTradingEngineTest
+    {
+        [TestMethod]
+        public void Debug()
+        {
+            var engine = new ContinuousTradingEngine();
+            engine.Start();
+
+            Thread.Sleep(120000);
+
+            engine.Stop();
+        }
+    }
+}
---
+++ AutomatedTrading/ContinuousTradingEngine.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Sonneville.PriceTools.Google;
+using Sonneville.PriceTools.Yahoo;
+
+namespace Sonneville.PriceTools.AutomatedTrading
+{
+    public class ContinuousTradingEngine
+    {
+        #region Private Members
+
+        private readonly object _syncroot = new object();
+        private readonly IList<string> _tradableTickers = new List<string> {"DE", "MSFT", "IBM", "GOOG", "AAPL"};
+        private CancellationTokenSource _cts;
+        private Task _engineTask;
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
+
+        #endregion
+
+        #region Client Control
+
+        public void Start()
+        {
+            lock(_syncroot)
+            {
+                if (_engineTask == null)
+                {
+                    _cts = new CancellationTokenSource();
+                    _engineTask = Task.Factory.StartNew(() => Execute(_cts.Token));
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock(_syncroot)
+            {
+                if (_engineTask != null)
+                {
+                    _cts.Cancel();
+                    try
+                    {
+                        _semaphore.Release();
+                        _engineTask.Wait();
+                    }
+                    finally
+                    {
+                        _cts = null;
+                        _engineTask = null;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        private void Execute(CancellationToken token)
+        {
+            SubscribeToPriceUpdates(token);
+            while (true)
+            {
+                if (token.IsCancellationRequested) break;
+
+                Iterate();
+                _semaphore.Wait(token);
+            }
+        }
+
+        private void SubscribeToPriceUpdates(CancellationToken token)
+        {
+            var provider = new YahooPriceDataProvider();
+            for (int i = 0; i < _tradableTickers.Count; i++)
+            {
+                if (i % 10 == 0 && token.IsCancellationRequested) return;
+
+                var priceSeries = PriceSeriesFactory.CreatePriceSeries(_tradableTickers[i]);
+                priceSeries.NewDataAvailable += (sender, e) => _semaphore.Release();
+                provider.StartAutoUpdate(priceSeries);
+            }
+        }
+
+        private void Iterate()
+        {
+            var message = String.Format("{0}: Iterating...", DateTime.Now);
+            Log(message);
+
+            // make money here
+            //throw new NotImplementedException();
+        }
+
+        private static void Log(string message)
+        {
+            using(var writer = new StreamWriter(File.OpenWrite(@"C:\Users\Mediacenter\Documents\output.txt")))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
