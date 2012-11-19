f27dbab185c045da2cea3cafef22cff55884ba9c
John Sonneville <SonnevilleJ@gmail.com>
2012-11-18 21:04:39 -0600

    Added ContinuousAnalyzer and ISignalProcessor.

---
+++ AutomatedTrading/ContinuousAnalyzer.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Sonneville.PriceTools.Data;
+
+namespace Sonneville.PriceTools.AutomatedTrading
+{
+    public abstract class ContinuousAnalyzer : IDisposable
+    {
+        #region Private Members
+
+        private readonly object _syncRoot = new object();
+
+        private readonly PriceDataProvider _priceDataProvider;
+        private readonly IPriceSeries _priceSeries;
+        private readonly ISignalProcessor _signalProcessor;
+
+        #endregion
+        
+        #region Constructors
+
+        protected ContinuousAnalyzer(IPriceSeries priceSeries, ISignalProcessor signalProcessor, PriceDataProvider priceDataProvider)
+        {
+            _priceSeries = priceSeries;
+            _signalProcessor = signalProcessor;
+            _priceDataProvider = priceDataProvider;
+        }
+
+        ~ContinuousAnalyzer()
+        {
+            Dispose();
+        }
+
+        #endregion
+
+        #region IDisposable Implementation
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        #endregion
+
+        protected IPriceSeries PriceSeries
+        {
+            get { return _priceSeries; }
+        }
+
+        protected PriceDataProvider PriceDataProvider
+        {
+            get { return _priceDataProvider; }
+        }
+
+        protected ISignalProcessor SignalProcessor
+        {
+            get { return _signalProcessor; }
+        }
+
+        private bool IsRunning { get; set; }
+
+        private CancellationTokenSource CancellationTokenSource { get; set; }
+
+        private Task Task { get; set; }
+
+        #region Control Methods
+
+        public void Start()
+        {
+            lock (_syncRoot)
+            {
+                if (IsRunning)
+                    throw new InvalidOperationException(
+                        "Cannot start execution of the ContinuousAnalyzer because it has already been started.");
+
+                CancellationTokenSource = new CancellationTokenSource();
+                var cancellationToken = CancellationTokenSource.Token;
+                cancellationToken.ThrowIfCancellationRequested();
+
+                IsRunning = true;
+                Task = new Task(() => Execute(cancellationToken), cancellationToken);
+
+                Task.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            if (CancellationTokenSource != null)
+            {
+                CancellationTokenSource.Cancel();
+                CancellationTokenSource = null;
+                Task = null;
+                IsRunning = false;
+            }
+        }
+
+        #endregion
+        
+        private void Execute(CancellationToken token)
+        {
+            var resetEvent = new AutoResetEvent(false);
+            EventHandler<NewDataAvailableEventArgs> handler = (source, eventArgs) => resetEvent.Set();
+
+            try
+            {
+                PriceSeries.NewDataAvailable += handler;
+                PriceDataProvider.StartAutoUpdate(_priceSeries);
+
+                while (true)
+                {
+                    resetEvent.WaitOne();
+
+                    token.ThrowIfCancellationRequested();
+                    AnalyzePeriodo();
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                PriceDataProvider.StopAutoUpdate(_priceSeries);
+                PriceSeries.NewDataAvailable -= handler;
+            }
+        }
+
+        protected abstract void AnalyzePeriodo();
+    }
+}
---
+++ AutomatedTrading/ISignalProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace Sonneville.PriceTools.AutomatedTrading
+{
+    /// <summary>
+    /// Processes signals for changes in price.
+    /// </summary>
+    public interface ISignalProcessor
+    {
+        /// <summary>
+        /// Signals a forthcoming change in price.
+        /// </summary>
+        /// <param name="priceSeries">The <see cref="IPriceSeries"/> which is expected to change.</param>
+        /// <param name="direction">The anticipated direction of the price change.</param>
+        /// <param name="magnitude">The anticipated percent change of the price.</param>
+        void Signal(IPriceSeries priceSeries, double direction, double magnitude);
+    }
+}
