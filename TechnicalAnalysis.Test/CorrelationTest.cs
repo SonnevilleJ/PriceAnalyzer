9cd915a3cea03714f5e170d796d872edc67dee08
John Sonneville <SonnevilleJ@gmail.com>
2013-04-21 14:33:27 -0500

    Implemented correlation indicator.

---
+++ TechnicalAnalysis.Test/CorrelationTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sonneville.PriceTools;
+using Sonneville.PriceTools.TechnicalAnalysis;
+using Sonneville.PriceTools.Test.PriceData;
+using Statistics;
+
+namespace Test.Sonneville.PriceTools.TechnicalAnalysis
+{
+    [TestClass]
+    public class CorrelationTest
+    {
+        [TestMethod]
+        public void CorrelationIbmDeereTest()
+        {
+            var ibm = TestPriceSeries.IBM_1_1_2011_to_3_15_2011_Daily_Yahoo_PS;
+            var de = TestPriceSeries.DE_1_1_2011_to_6_30_2011;
+
+            var ibmDecimals = ibm.GetPreviousTimePeriods(20, ibm.Tail).Select(x => x.Value());
+            var deDecimals = de.GetPreviousTimePeriods(20, ibm.Tail).Select(x => x.Value());
+
+            var expected = ibmDecimals.Correlation(deDecimals);
+            var actual = new Correlation(ibm, 20, de)[ibm.Tail];
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- TechnicalAnalysis/Correlation.cs
+++ TechnicalAnalysis/Correlation.cs
@@ -1,92 +1,99 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Statistics;
 
-//namespace Sonneville.PriceTools.TechnicalAnalysis
-//{
-//    /// <summary>
-//    /// Indicates the level of correlation between two <see cref="ITimePeriod"/>.
-//    /// </summary>
-//    public class Correlation : TimeSeriesIndicator
-//    {
-//        //
-//        // The algorithms in the Correlation class are based on the following article:
-//        // http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:correlation_coeffici
-//        //
+namespace Sonneville.PriceTools.TechnicalAnalysis
+{
+    /// <summary>
+    /// Indicates the level of correlation between two <see cref="ITimePeriod"/>.
+    /// </summary>
+    public class Correlation : TimeSeriesIndicator
+    {
+        //
+        // The algorithms in the Correlation class are based on the following article:
+        // http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:correlation_coeffici
+        //
 
-//        #region Private Members
+        #region Private Members
 
-//        private readonly ITimePeriod _target;
+        private readonly ITimeSeries _target;
 
-//        #endregion
+        #endregion
 
-//        #region Constructors
+        #region Constructors
 
-//        public Correlation(IPriceSeries priceSeries, int lookback, ITimePeriod target)
-//            : base(priceSeries, lookback)
-//        {
-//            _target = target;
-//        }
+        public Correlation(IPriceSeries priceSeries, int lookback, ITimeSeries target)
+            : base(priceSeries, lookback)
+        {
+            _target = target;
+        }
 
-//        #endregion
+        #endregion
 
-//        /// <summary>
-//        /// Calculates the correlation coefficient for a single period.
-//        /// </summary>
-//        /// <param name="index">The index of the value to calculate.</param>
-//        protected decimal? Calculate(int index)
-//        {
-//            //var timeSeriesValues = GetValues(MeasuredTimeSeries, index);
-//            //var targetValues = GetValues(_target, index);
+        protected override decimal Calculate(DateTime index)
+        {
+            var myPeriods = MeasuredTimeSeries.GetPreviousTimePeriods(Lookback, index).Select(x => x[index]);
+            var otherPeriods = _target.GetPreviousTimePeriods(Lookback, index).Select(x => x[index]);
+            return myPeriods.Correlation(otherPeriods);
+        }
 
-//            //var timeSeriesTask = new Task<IList<decimal>>(() => SquareElements(index, timeSeriesValues));
-//            //var targetTask = new Task<IList<decimal>>(() => SquareElements(index, targetValues));
-//            //Task.WaitAll(timeSeriesTask, targetTask);
+        ///// <summary>
+        ///// Calculates the correlation coefficient for a single period.
+        ///// </summary>
+        ///// <param name="index">The index of the value to calculate.</param>
+        //protected decimal? Calculate(int index)
+        //{
+        //    var timeSeriesValues = GetValues(MeasuredTimeSeries, index);
+        //    var targetValues = GetValues(_target, index);
 
-//            //var timeSeriesSquares = timeSeriesTask.Result;
-//            //var targetSquares = targetTask.Result;
+        //    var timeSeriesTask = new Task<IList<decimal>>(() => SquareElements(index, timeSeriesValues));
+        //    var targetTask = new Task<IList<decimal>>(() => SquareElements(index, targetValues));
+        //    Task.WaitAll(timeSeriesTask, targetTask);
 
-//            //var squares = MultiplyElements(index, timeSeriesSquares, targetSquares);
+        //    var timeSeriesSquares = timeSeriesTask.Result;
+        //    var targetSquares = targetTask.Result;
 
-//            //var timeSeriesSquare = timeSeriesValues.Average();
-//            //var timeSeriesVariance = timeSeriesSquares.Average() - (timeSeriesSquare*timeSeriesSquare);
-//            //var targetSquare = targetValues.Average();
-//            //var targetVariance = targetSquares.Average() - (targetSquare*targetSquare);
-//            //var covariance = squares.Average() - (timeSeriesSquare*targetSquare);
-//            //return covariance/(decimal) Math.Sqrt((double) (timeSeriesVariance*targetVariance));
+        //    var squares = MultiplyElements(index, timeSeriesSquares, targetSquares);
 
-//            throw new NotImplementedException();
-//        }
+        //    var timeSeriesSquare = timeSeriesValues.Average();
+        //    var timeSeriesVariance = timeSeriesSquares.Average() - (timeSeriesSquare * timeSeriesSquare);
+        //    var targetSquare = targetValues.Average();
+        //    var targetVariance = targetSquares.Average() - (targetSquare * targetSquare);
+        //    var covariance = squares.Average() - (timeSeriesSquare * targetSquare);
+        //    return covariance / (decimal)Math.Sqrt((double)(timeSeriesVariance * targetVariance));
 
-//        private IList<decimal> GetValues(ITimePeriod timePeriod, int index)
-//        {
-//            var result = new List<decimal>();
-//            //for (var i = index - (Lookback - 1); i <= index; i++)
-//            //{
-//            //    result.Add(timePeriod[ConvertIndexToDateTime(i)]);
-//            //}
-//            return result;
-//        }
+        //    throw new NotImplementedException();
+        //}
 
-//        private IList<decimal> SquareElements(int index, IList<decimal> series)
-//        {
-//            return MultiplyElements(index, series, series);
-//        }
+        //private IList<decimal> GetValues(ITimePeriod timePeriod, int index)
+        //{
+        //    var result = new List<decimal>();
+        //    for (var i = index - (Lookback - 1); i <= index; i++)
+        //    {
+        //        result.Add(timePeriod[ConvertIndexToDateTime(i)]);
+        //    }
+        //    return result;
+        //}
 
-//        private IList<decimal> MultiplyElements(int index, IList<decimal> series1, IList<decimal> series2)
-//        {
-//            var count = Lookback - 1;
-//            if (index >= count)
-//            {
-//                var result = new List<decimal>();
-//                for (var i = index - count; i <= index; i++)
-//                {
-//                    result.Add(series1[i] * series2[i]);
-//                }
-//                return result;
-//            }
-//            return null;
-//        }
-//    }
-//}
+        //private IList<decimal> SquareElements(int index, IList<decimal> series)
+        //{
+        //    return MultiplyElements(index, series, series);
+        //}
+
+        //private IList<decimal> MultiplyElements(int index, IList<decimal> series1, IList<decimal> series2)
+        //{
+        //    var count = Lookback - 1;
+        //    if (index >= count)
+        //    {
+        //        var result = new List<decimal>();
+        //        for (var i = index - count; i <= index; i++)
+        //        {
+        //            result.Add(series1[i] * series2[i]);
+        //        }
+        //        return result;
+        //    }
+        //    return null;
+        //}
+    }
+}
