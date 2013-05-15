56e34e8b8b4ba353cb2ca6b21198ff2c3f6c837e
John Sonneville <SonnevilleJ@gmail.com>
2013-05-14 21:17:29 -0700

    Throw exception for unimplemented resolutions.

--- PriceTools/Extensions/DateTimeExtensions.cs
+++ PriceTools/Extensions/DateTimeExtensions.cs
@@ -114,12 +114,15 @@ namespace Sonneville.PriceTools
         {
             // Weeks, Months, Years, etc do not have "trading periods"
             if (resolution >= Resolution.Weeks) return true;
-            
-            return dateTime.DayOfWeek == DayOfWeek.Monday ||
-                   dateTime.DayOfWeek == DayOfWeek.Tuesday ||
-                   dateTime.DayOfWeek == DayOfWeek.Wednesday ||
-                   dateTime.DayOfWeek == DayOfWeek.Thursday ||
-                   dateTime.DayOfWeek == DayOfWeek.Friday;
+
+            if (resolution >= Resolution.Days)
+                return dateTime.DayOfWeek == DayOfWeek.Monday ||
+                       dateTime.DayOfWeek == DayOfWeek.Tuesday ||
+                       dateTime.DayOfWeek == DayOfWeek.Wednesday ||
+                       dateTime.DayOfWeek == DayOfWeek.Thursday ||
+                       dateTime.DayOfWeek == DayOfWeek.Friday;
+
+            throw new NotImplementedException();
         }
 
         /// <summary>
