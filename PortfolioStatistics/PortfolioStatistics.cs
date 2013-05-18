2f16837e1179ce2e5a3064115709b14a6ed6c092
John Sonneville <SonnevilleJ@gmail.com>
2013-05-17 20:02:41 -0800

    Refactored KellyPercentage to be more functional.

--- PortfolioStatistics/PortfolioStatistics.cs
+++ PortfolioStatistics/PortfolioStatistics.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Sonneville.PriceTools.PortfolioStatistics
@@ -8,31 +7,16 @@ namespace Sonneville.PriceTools.PortfolioStatistics
     {
         public static decimal KellyPercentage(this IEnumerable<IHolding> holdings)
         {
-            long wins = 0;
-            long losses = 0;
-            decimal totalGain = 0;
-            decimal totalLoss = 0;
-            decimal trades = 0;
+            var netProfits = holdings.Select(holding => holding.NetProfit());
+            var wins = netProfits.Where(x => x > 0);
+            var losses = netProfits.Where(x => x <= 0);
+            var totalGain = wins.Sum(x => x);
+            var totalLoss = 0 - losses.Sum(x => x);
 
-            foreach (var netProfit in holdings.Select(holding => holding.NetProfit()))
-            {
-                if (netProfit > 0)
-                {
-                    wins++;
-                    totalGain += netProfit;
-                }
-                else
-                {
-                    losses++;
-                    totalLoss += Math.Abs(netProfit);
-                }
-                trades++;
-            }
-            
-            var winPercent = wins/trades;
-            var lossPercent = losses/trades;
-            var averageGain = totalGain/wins;
-            var averageLoss = totalLoss/losses;
+            var winPercent = wins.Count()/(decimal) netProfits.Count();
+            var lossPercent = losses.Count()/(decimal) netProfits.Count();
+            var averageGain = totalGain/wins.Count();
+            var averageLoss = totalLoss/losses.Count();
             var ratio = averageGain/averageLoss;
 
             return winPercent - (lossPercent/ratio);
