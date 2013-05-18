caef650b1fac6815a43a614310a97ad666c39908
John Sonneville <SonnevilleJ@gmail.com>
2013-05-17 16:05:57 -0800

    More refactoring of PriceDataProviderTest.

--- Data.Test/PriceDataProviderTest.cs
+++ Data.Test/PriceDataProviderTest.cs
@@ -28,87 +28,95 @@ namespace Test.Sonneville.PriceTools.Data
         [TestMethod]
         public void DailyDownloadSingleDay()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2012, 8, 7);
             var tail = new DateTime(2012, 8, 7).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Days);
 
-            Assert.AreEqual(1, _priceSeries.PricePeriods.Count());
+            DownloadPeriodsTest(head, tail, 1, Resolution.Days);
         }
 
         [TestMethod]
-        public void DailyDownloadTestResolution()
+        public void DailyDownloadResolution()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2011, 1, 3);
             var tail = new DateTime(2011, 3, 15).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Days);
+            var minTimeSpan = new TimeSpan(0, 0, 1);
+            var maxTimeSpan = new TimeSpan(24, 0, 0);
 
-            Assert.AreEqual(Resolution.Days, _priceSeries.Resolution);
-            foreach (var period in _priceSeries.PricePeriods)
-            {
-                Assert.IsTrue(period.Tail - period.Head < new TimeSpan(24, 0, 0));
-            }
+            DownloadResolutionTest(head, tail, minTimeSpan, maxTimeSpan, Resolution.Days);
         }
 
         [TestMethod]
-        public void DailyDownloadTestPeriods()
+        public void DailyDownloadPeriods()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2011, 1, 3);
             var tail = new DateTime(2011, 3, 15).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Days);
 
-            Assert.AreEqual(50, _priceSeries.PricePeriods.Count());
+            DownloadPeriodsTest(head, tail, 50, Resolution.Days);
         }
 
         [TestMethod]
         public void DailyDownloadDates()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2011, 1, 3);
             var tail = new DateTime(2011, 3, 15).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Days);
 
-            Assert.AreEqual(head, _priceSeries.Head);
-            Assert.AreEqual(tail, _priceSeries.Tail);
+            DownloadDatesTest(head, tail, Resolution.Days);
         }
 
         [TestMethod]
         public void WeeklyDownloadPeriods()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2011, 1, 3);
             var tail = new DateTime(2011, 3, 15).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Weeks);
 
-            Assert.AreEqual(11, _priceSeries.PricePeriods.Count());
+            DownloadPeriodsTest(head, tail, 11, Resolution.Weeks);
         }
 
         [TestMethod]
         public void WeeklyDownloadResolution()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2011, 1, 3);
             var tail = new DateTime(2011, 3, 15).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Weeks);
+            var minTimeSpan = new TimeSpan(7, 0, 0, 0);
+            var maxTimeSpan = new TimeSpan(23, 59, 59);
 
-            Assert.AreEqual(Resolution.Weeks, _priceSeries.Resolution);
-            var periods = _priceSeries.PricePeriods.ToArray();
-            for (var i = 1; i < periods.Count() - 1; i++) // skip check on first and last periods
-            {
-                Assert.IsTrue(periods[i].Tail - periods[i].Head >= new TimeSpan(23, 59, 59));
-                Assert.IsTrue(periods[i].Tail - periods[i].Head < new TimeSpan(7, 0, 0, 0));
-            }
+            DownloadResolutionTest(head, tail, minTimeSpan, maxTimeSpan, Resolution.Weeks);
         }
 
         [TestMethod]
         public void WeeklyDownloadDates()
         {
-            var provider = GetTestObjectInstance();
             var head = new DateTime(2011, 1, 3);
             var tail = new DateTime(2011, 3, 15).CurrentPeriodClose(Resolution.Days);
-            provider.UpdatePriceSeries(_priceSeries, head, tail, Resolution.Weeks);
+
+            DownloadDatesTest(head, tail, Resolution.Weeks);
+        }
+
+        private void DownloadPeriodsTest(DateTime head, DateTime tail, int expected, Resolution resolution)
+        {
+            var provider = GetTestObjectInstance();
+            provider.UpdatePriceSeries(_priceSeries, head, tail, resolution);
+            Assert.AreEqual(expected, _priceSeries.PricePeriods.Count());
+        }
+
+        private void DownloadResolutionTest(DateTime head, DateTime tail, TimeSpan minTimeSpan, TimeSpan maxTimeSpan, Resolution resolution)
+        {
+            var provider = GetTestObjectInstance();
+            provider.UpdatePriceSeries(_priceSeries, head, tail, resolution);
+
+            Assert.AreEqual(resolution, _priceSeries.Resolution);
+            var periods = _priceSeries.PricePeriods.ToArray();
+            for (var i = 1; i < periods.Count() - 1; i++) // skip check on first and last periods
+            {
+                Assert.IsTrue(periods[i].Tail - periods[i].Head >= minTimeSpan);
+                Assert.IsTrue(periods[i].Tail - periods[i].Head < maxTimeSpan);
+            }
+        }
+
+        private void DownloadDatesTest(DateTime head, DateTime tail, Resolution resolution)
+        {
+            var provider = GetTestObjectInstance();
+            provider.UpdatePriceSeries(_priceSeries, head, tail, resolution);
 
             Assert.AreEqual(head, _priceSeries.Head);
             Assert.AreEqual(tail, _priceSeries.Tail);
