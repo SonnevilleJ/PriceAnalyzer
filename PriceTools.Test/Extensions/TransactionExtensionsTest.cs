e75917d6edf2460e4261bf487b9489188a765d4c
John Sonneville <SonnevilleJ@gmail.com>
2013-09-15 22:02:01 -0500

    Added IsOpeningTransaction.

---
+++ PriceTools.Test/Extensions/TransactionExtensionsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sonneville.PriceTools;
+
+namespace Test.Sonneville.PriceTools.Extensions
+{
+    [TestClass]
+    public class TransactionExtensionsTest
+    {
+        [TestMethod]
+        public void Buy()
+        {
+            InnerTest(OrderType.Buy, true);
+        }
+        
+        [TestMethod]
+        public void Sell()
+        {
+            InnerTest(OrderType.Sell, false);
+        }
+        
+        [TestMethod]
+        public void SellShort()
+        {
+            InnerTest(OrderType.SellShort, true);
+        }
+        
+        [TestMethod]
+        public void BuyToCover()
+        {
+            InnerTest(OrderType.BuyToCover, false);
+        }
+        
+        [TestMethod]
+        public void DividendReinvestment()
+        {
+            InnerTest(OrderType.DividendReinvestment, true);
+        }
+        
+        [TestMethod]
+        public void DividendReceipt()
+        {
+            InnerTest(OrderType.DividendReceipt, false);
+        }
+        
+        [TestMethod]
+        public void Deposit()
+        {
+            InnerTest(OrderType.Deposit, false);
+        }
+        
+        [TestMethod]
+        public void Withdrawal()
+        {
+            InnerTest(OrderType.Withdrawal, false);
+        }
+        
+        private static void InnerTest(OrderType orderType, bool expected)
+        {
+            var transaction = new TransactionFactory().ConstructTransaction(orderType, DateTime.Today, "DE", 5, 5, 0);
+
+            Assert.AreEqual(expected, transaction.IsOpeningTransaction());
+        }
+    }
+}
---
+++ PriceTools/Extensions/TransactionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Sonneville.PriceTools.Implementation;
+
+namespace Sonneville.PriceTools
+{
+    public static class TransactionExtensions
+    {
+        private static readonly Dictionary<Type, bool> Dictionary = new Dictionary<Type, bool>();
+
+        static TransactionExtensions()
+        {
+            Dictionary.Add(typeof (Buy), true);
+            Dictionary.Add(typeof (SellShort), true);
+            Dictionary.Add(typeof (DividendReinvestment), true);
+
+            Dictionary.Add(typeof (Sell), false);
+            Dictionary.Add(typeof (BuyToCover), false);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not a transaction opens or increases an investment in a position.
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
+        public static bool IsOpeningTransaction(this Transaction transaction)
+        {
+            try
+            {
+                return Dictionary[transaction.GetType()];
+            }
+            catch (KeyNotFoundException)
+            {
+                return false;
+            }
+        }
+    }
+}
