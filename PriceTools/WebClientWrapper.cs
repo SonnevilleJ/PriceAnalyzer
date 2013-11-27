8b75cb76fbec44f90d838df68c39d1f388e34f75
John Sonneville <SonnevilleJ@gmail.com>
2013-11-27 16:57:57 -0600

    Added XML doc comments.

--- PriceTools/WebClientWrapper.cs
+++ PriceTools/WebClientWrapper.cs
@@ -3,15 +3,24 @@ using System.Net;
 
 namespace Sonneville.PriceTools
 {
+    /// <summary>
+    /// Wraps a <see cref="System.Net.WebClient"/> in an <see cref="IWebClient"/>.
+    /// </summary>
     public class WebClientWrapper : IWebClient
     {
         private readonly WebClient _webClient;
 
+        /// <summary>
+        /// Creates a <see cref="System.Net.WebClient"/>.
+        /// </summary>
         public WebClientWrapper()
         {
             _webClient = new WebClient {Proxy = {Credentials = CredentialCache.DefaultNetworkCredentials}};
         }
 
+        /// <summary>
+        /// Opens a readable stream for the data downloaded from a resource with the URI specified as a <see cref="T:System.Uri"/>
+        /// </summary>
         public Stream OpenRead(string address)
         {
             return _webClient.OpenRead(address);
