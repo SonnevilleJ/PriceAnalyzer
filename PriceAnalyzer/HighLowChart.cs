ae8f95c8c3579646ba43c0d2dec9c69277b9ccd6
John Sonneville <SonnevilleJ@gmail.com>
2014-05-28 22:31:08 -0500

    Added horizontal axis.

--- PriceAnalyzer/HighLowChart.cs
+++ PriceAnalyzer/HighLowChart.cs
@@ -16,6 +16,7 @@ namespace Sonneville.PriceTools.PriceAnalyzer
         private IList<IPricePeriod> _pricePeriods;
         private readonly Canvas _canvas = new Canvas();
         private readonly Canvas _verticalCanvas = new Canvas();
+        private readonly Canvas _horizontalCanvas = new Canvas();
 
         public HighLowChart()
         {
@@ -23,12 +24,20 @@ namespace Sonneville.PriceTools.PriceAnalyzer
             column1.Width = new GridLength(45);
             ColumnDefinitions.Add(column1);
             ColumnDefinitions.Add(new ColumnDefinition());
+            RowDefinitions.Add(new RowDefinition());
+            var row2 = new RowDefinition();
+            row2.Height = new GridLength(20);
+            RowDefinitions.Add(row2);
             Grid.SetColumn(_verticalCanvas, 0);
+            Grid.SetRow(_verticalCanvas, 0);
             Grid.SetColumn(_canvas, 1);
-
+            Grid.SetRow(_canvas, 0);
+            Grid.SetColumn(_horizontalCanvas, 1);
+            Grid.SetRow(_horizontalCanvas, 1);
 
             Children.Add(_canvas);
             Children.Add(_verticalCanvas);
+            Children.Add(_horizontalCanvas);
             SizeChanged += delegate { if (_pricePeriods != null) DrawChart(); };
         }
 
@@ -44,12 +53,55 @@ namespace Sonneville.PriceTools.PriceAnalyzer
             var lowestLow = (double) _pricePeriods.Min(pricePeriod => pricePeriod.Low);
 
             var dollarRange = (highestHigh - lowestLow);
-            var pixelsPerDollar = (ActualHeight/dollarRange);
-            DrawCanvas(highestHigh, lowestLow, pixelsPerDollar);
-            VerticalAxis(highestHigh, lowestLow, pixelsPerDollar);
+            var pixelsPerDollar = (_canvas.ActualHeight/dollarRange);
+            DrawVerticalAxis(highestHigh, lowestLow, pixelsPerDollar);
+            var firstDay = _pricePeriods.Min(pricePeriod => pricePeriod.Head);
+            var lastDay = _pricePeriods.Max(pricePeriod => pricePeriod.Tail);
+            var pixelsPerDay = _canvas.ActualWidth / (lastDay - firstDay).Days;
+            DrawHorizontalAxis(firstDay, lastDay, pixelsPerDay);
+            DrawCanvas(highestHigh, lowestLow, pixelsPerDollar, pixelsPerDay);
         }
 
-        private void VerticalAxis(double highestHigh, double lowestLow, double pixelsPerDollar)
+        private void DrawHorizontalAxis(DateTime firstDay, DateTime lastDay, double pixelsPerDay)
+        {
+            _horizontalCanvas.Children.Clear();
+
+            const int minimumPixelsPerTick = 60;
+            const double maximumTicks = 10;
+            var pixelsPerTick = Math.Max(_horizontalCanvas.ActualWidth / maximumTicks, minimumPixelsPerTick);
+            var periodsBetweenHorizontalTicks = pixelsPerTick / pixelsPerDay;
+            for (DateTime i = firstDay; i < lastDay; i = i.AddDays(periodsBetweenHorizontalTicks))
+            {
+                DrawHorizontalTick(pixelsPerDay, i, firstDay);
+            }
+            var separator = new Line();
+            separator.Y1 = 0;
+            separator.X1 = 0;
+            separator.X2 = _horizontalCanvas.ActualWidth;
+            separator.Y2 = 0;
+            separator.Stroke = Brushes.DarkBlue;
+            _horizontalCanvas.Children.Add(separator);
+        }
+
+        private void DrawHorizontalTick(double pixelsPerDay, DateTime tickDay, DateTime firstDay)
+        {
+            var line = new Line();
+            line.Y1 = 0;
+            line.Y2 = 5;
+            line.X1 = (tickDay - firstDay).Days*pixelsPerDay;
+            line.X2 = (tickDay - firstDay).Days*pixelsPerDay;
+            line.Stroke = Brushes.Black;
+            _horizontalCanvas.Children.Add(line);
+            var textBlock = new TextBlock();
+            textBlock.Text = tickDay.ToShortDateString();
+            textBlock.Foreground = Brushes.Black;
+            Canvas.SetTop(textBlock, 7);
+            Canvas.SetLeft(textBlock, line.X1 - 25);
+
+            _horizontalCanvas.Children.Add(textBlock);
+        }
+
+        private void DrawVerticalAxis(double highestHigh, double lowestLow, double pixelsPerDollar)
         {
             _verticalCanvas.Children.Clear();
 
@@ -88,18 +140,17 @@ namespace Sonneville.PriceTools.PriceAnalyzer
             _verticalCanvas.Children.Add(line);
         }
 
-        private void DrawCanvas(double maxYdollar, double minYdollar, double pixelsPerDollar)
+        private void DrawCanvas(double maxYdollar, double minYdollar, double pixelsPerDollar, double pixelsPerDay)
         {
             _canvas.Children.Clear();
 
             var minX = _pricePeriods.First().Head;
-            var maxX = _pricePeriods.Last().Tail;
 
             foreach (var pricePeriod in _pricePeriods)
             {
                 var line = new Line();
-                line.X1 = ((pricePeriod.Head - minX).Days*5) + 5;
-                line.Y1 = ActualHeight - ((double) pricePeriod.Low - minYdollar)*pixelsPerDollar;
+                line.X1 = ((pricePeriod.Head - minX).Days*pixelsPerDay);
+                line.Y1 = _canvas.ActualHeight - ((double) pricePeriod.Low - minYdollar)*pixelsPerDollar;
                 line.X2 = line.X1;
                 line.Y2 = (maxYdollar - (double) pricePeriod.High)*pixelsPerDollar;
                 line.Stroke = Brushes.Blue;
