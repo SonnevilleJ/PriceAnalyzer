030cdc6ea1d78c5f0423a9b9ff0e91f602ea112e
John Sonneville <SonnevilleJ@gmail.com>
2014-05-28 19:40:14 -0500

    Fixed null reference exception.

--- PriceAnalyzer/ChartForm.cs
+++ PriceAnalyzer/ChartForm.cs
@@ -4,6 +4,11 @@ namespace Sonneville.PriceTools.PriceAnalyzer
 {
     public partial class ChartForm : GenericForm
     {
+        public ChartForm()
+        {
+            InitializeComponent();
+        }
+
         protected override void DisplayContent(IList<IPricePeriod> pricePeriods, string ticker)
         {
             highLowChart1.DrawPricePeriods(pricePeriods);
