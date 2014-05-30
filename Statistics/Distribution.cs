98084a2842837b79c01ecfb5f4031c2acd682ad1
John Sonneville <SonnevilleJ@gmail.com>
2014-05-30 00:47:24 -0500

    Added inverse normal distribution statistical calculation for use with a Monte Carlo system.

---
+++ Statistics/Distribution.cs
@@ -0,0 +1,12 @@
+﻿using MathNet.Numerics.Distributions;
+
+namespace Sonneville.Statistics
+{
+    public class Distribution
+    {
+        public double FindInverseNormal(double probability, double mean, double standardDeviation)
+        {
+            return new Normal(mean, standardDeviation).InverseCumulativeDistribution(probability);
+        }
+    }
+}
