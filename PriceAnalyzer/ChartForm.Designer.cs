138b4ea9920e809fb324ec9675e224c9d432277f
John Sonneville <SonnevilleJ@gmail.com>
2014-06-04 22:03:28 -0500

    Removed inheritance from ChartForm.

--- PriceAnalyzer/ChartForm.Designer.cs
+++ PriceAnalyzer/ChartForm.Designer.cs
@@ -28,6 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.importButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.TickerTextBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.highLowChart1 = new Sonneville.PriceTools.PriceAnalyzer.HighLowChart();
             this.tabControl1 = new System.Windows.Forms.TabControl();
@@ -95,7 +102,88 @@
             this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
-
+            // 
+            // importButton
+            // 
+            this.importButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.importButton.Location = new System.Drawing.Point(657, 330);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(75, 23);
+            this.importButton.TabIndex = 1;
+            this.importButton.Text = "Import...";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.ImportClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "CSV files|*.csv";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Location = new System.Drawing.Point(738, 330);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.CloseClick);
+            // 
+            // TickerTextBox
+            // 
+            this.TickerTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.TickerTextBox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.TickerTextBox.Location = new System.Drawing.Point(12, 332);
+            this.TickerTextBox.Name = "TickerTextBox";
+            this.TickerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TickerTextBox.TabIndex = 4;
+            this.TickerTextBox.Text = "DE";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.downloadButton.Location = new System.Drawing.Point(322, 330);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 5;
+            this.downloadButton.Text = "Download...";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.endDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.endDateTimePicker.Location = new System.Drawing.Point(220, 332);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(96, 20);
+            this.endDateTimePicker.TabIndex = 6;
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.startDateTimePicker.Location = new System.Drawing.Point(118, 332);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(96, 20);
+            this.startDateTimePicker.TabIndex = 6;
+            // 
+            // GenericForm
+            // 
+            this.AcceptButton = this.importButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 365);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.TickerTextBox);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.importButton);
+            this.Name = "GenericForm";
+            this.Text = "PriceAnalyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
@@ -105,6 +193,13 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button addTabButton;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox TickerTextBox;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
     }
 }
 
--- PriceAnalyzer/ChartForm.cs
+++ PriceAnalyzer/ChartForm.cs
@@ -1,17 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 using System.Windows.Forms.Integration;
 
 namespace Sonneville.PriceTools.PriceAnalyzer
 {
-    public partial class ChartForm : GenericForm
+    public partial class ChartForm : Form
     {
+        private readonly PriceDataManager _priceDataManager = new PriceDataManager();
+
         public ChartForm()
         {
             InitializeComponent();
+
+            startDateTimePicker.Value = DateTime.Now.AddMonths(-1);
         }
 
-        protected override void DisplayContent(IList<IPricePeriod> pricePeriods, string ticker)
+        private void ImportClick(object sender, EventArgs e)
+        {
+            var dialogResult = openFileDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var fullFileName = openFileDialog1.FileName;
+                var pricePeriods = _priceDataManager.ParseCsvFile(fullFileName);
+                var fileName = new FileInfo(fullFileName).Name;
+                var tabName = fileName.Substring(0, fileName.IndexOf("."));
+                DisplayContent(pricePeriods, tabName);
+            }
+        }
+
+        private void CloseClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            downloadButton.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+
+            var ticker = TickerTextBox.Text;
+            DisplayContent(_priceDataManager.DownloadPricePeriods(ticker, startDateTimePicker.Value, endDateTimePicker.Value), ticker);
+
+            downloadButton.Enabled = true;
+            this.Cursor = Cursors.Default;
+        }
+
+        private void DisplayContent(IList<IPricePeriod> pricePeriods, string ticker)
         {
             var currentTab = this.tabControl1.SelectedTab;
             var currentElementHost = (ElementHost)currentTab.Controls[0];
