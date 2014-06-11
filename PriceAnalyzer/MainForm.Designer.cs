c8a151a2a3e54fddb2de513c3439f169fd6b93aa
John Sonneville <SonnevilleJ@gmail.com>
2014-06-10 22:36:30 -0500

    Fixed bug - maximizing window did not cause chart to resize.

--- PriceAnalyzer/MainForm.Designer.cs
+++ PriceAnalyzer/MainForm.Designer.cs
@@ -53,6 +53,9 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
