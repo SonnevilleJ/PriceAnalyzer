1c742561dfd3584a48cd7d05bb9cc696abb4c415
John Sonneville <SonnevilleJ@gmail.com>
2014-07-14 21:01:07 -0500

    Added color to price periods.

--- PriceAnalyzer/HighLowChart.cs
+++ PriceAnalyzer/HighLowChart.cs
@@ -124,19 +124,34 @@ namespace Sonneville.PriceTools.PriceAnalyzer
             _canvas.Children.Clear();
 
             var minX = _pricePeriods.First().Head;
-
+            decimal priorPeriodClose = 0;
+            
             foreach (var pricePeriod in _pricePeriods)
             {
+                var closeColorBrush = CloseColorBrush(priorPeriodClose, pricePeriod.Close);
                 var x = ((pricePeriod.Head - minX).Days*pixelsPerDay) + (.5*pixelsPerDay);
-                var highLowBar = CreateLine(x, _canvas.ActualHeight - ((double) pricePeriod.Low - minYdollar)*pixelsPerDollar, x, (maxYdollar - (double) pricePeriod.High)*pixelsPerDollar, Brushes.Blue);
+                var highLowBar = CreateLine(x, _canvas.ActualHeight - ((double) pricePeriod.Low - minYdollar)*pixelsPerDollar, x, (maxYdollar - (double) pricePeriod.High)*pixelsPerDollar, closeColorBrush);
                 var openY = _canvas.ActualHeight - ((double)pricePeriod.Open - minYdollar)*pixelsPerDollar;
-                var openBar = CreateLine(x - (pixelsPerDay*.25), openY, x, openY, Brushes.Blue);
+                var openBar = CreateLine(x - (pixelsPerDay*.25), openY, x, openY, closeColorBrush);
                 var closeY = _canvas.ActualHeight - ((double)pricePeriod.Close - minYdollar) * pixelsPerDollar;
-                var closeBar = CreateLine(x + (pixelsPerDay*.25), closeY, x, closeY, Brushes.Blue);
+                var closeBar = CreateLine(x + (pixelsPerDay*.25), closeY, x, closeY, closeColorBrush);
+                priorPeriodClose = pricePeriod.Close;
 
                 _canvas.Children.Add(highLowBar);
                 _canvas.Children.Add(openBar);
                 _canvas.Children.Add(closeBar);
+            }
+        }
+
+        private static Brush CloseColorBrush(decimal priorPeriodClose, decimal currentPeriodClose)
+        {
+            if (priorPeriodClose < currentPeriodClose)
+            {
+                return Brushes.Black;
+            }
+            else
+            {
+                return Brushes.Firebrick;
             }
         }
 
